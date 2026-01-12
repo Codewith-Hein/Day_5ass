66affc422c7907bf2f3d9878919716b11d9fa627
codewith-hein <codewithhein2@gmail.com>
2026-01-12 15:47:33 +0630

    update html css

---
+++ script.js
@@ -0,0 +1,9 @@
+let munu = document.querySelector(".menu");
+let menu_list = document.querySelector("#menu_li")
+
+
+
+
+munu.addEventListener("click", () => {
+    menu_list.classList.toggle("show")
+})
